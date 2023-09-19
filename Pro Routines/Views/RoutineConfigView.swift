5eba388d2e0cdac4485b40ad076c264e71d6b6c1
Jorge Felix <jfelixdev@icloud.com>
2023-09-19 14:51:09 -0500

    Added routine config view

---
+++ Pro Routines/Views/RoutineConfigView.swift
@@ -0,0 +1,66 @@
+//
+//  RoutineConfigView.swift
+//  Pro Routines
+//
+//  Created by Jorge Felix on 9/19/23.
+//
+
+import SwiftUI
+
+struct RoutineConfigView: View {
+    @Binding var routine: ProRoutine
+    @State private var newTaskName = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Details")) {
+                TextField("Title", text: $routine.title)
+                HStack {
+                    Slider(value: $routine.lengthInMinutesAsDouble, in: 5...20, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityValue("\(routine.lengthInMinutes) minutes")
+                    Spacer()
+                    Text("\(routine.lengthInMinutes)")
+                        .accessibilityHidden(true)
+                }
+                Picker("Theme", selection: $routine.theme) {
+                    ForEach(Theme.allCases, id: \.self) {
+                        Text(String("\($0)"))
+                    }
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(routine.tasks) { t in
+                    Text(t.name)
+                }
+                .onDelete { i in
+                    routine.tasks.remove(atOffsets: i)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newTaskName)
+                    Button(action: {
+                        withAnimation {
+                            let newTask = ProRoutine.Task(name: newTaskName)
+                            routine.tasks.append(newTask)
+                            newTaskName = ""
+                        }
+
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newTaskName.isEmpty)
+
+                }
+
+            }
+        }
+    }
+}
+
+struct RoutineConfigView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoutineConfigView(routine: .constant(ProRoutine.sampleData[0]))
+    }
+}
