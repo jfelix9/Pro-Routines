aa7b3093f4ba972cebb9729afe190f7492bf72bc
Jorge Felix <jfelixdev@icloud.com>
2023-09-19 11:32:34 -0500

    Initial Commit

---
+++ Pro Routines/Pro_RoutinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pro_RoutinesApp.swift
+//  Pro Routines
+//
+//  Created by Jorge Felix on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Pro_RoutinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
